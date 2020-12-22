52149d27adf4d308e97639d835289ff034ae2093
Thomas Schwinge <thomas@codesourcery.com>
2020-12-22 14:59:56 +0100

    [as, ld, run] Add testcases for '--help', '--version' options

--- test/lit.cfg.py
+++ test/lit.cfg.py
@@ -10,3 +10,15 @@ config.test_format = lit.formats.ShTest(True)
 config.test_source_root = os.path.dirname(__file__)
 
 config.suffixes = ['.test']
+
+
+config.substitutions.append(('%target_as_cmd', config.target_as))
+
+
+config.substitutions.append(('%target_ld_cmd', config.target_ld))
+
+
+if config.target_run:
+    config.available_features.add('target_run')
+
+    config.substitutions.append(('%target_run_cmd', config.target_run))

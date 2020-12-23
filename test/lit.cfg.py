b4e1cbf0d4838ac3f1e33843e84a3f0252755f78
Thomas Schwinge <thomas@codesourcery.com>
2020-12-23 21:47:26 +0100

    [ar, ranlib] Add testcases for '--help', '--version' options [#4]
    Some XFAILed.

--- test/lit.cfg.py
+++ test/lit.cfg.py
@@ -12,10 +12,16 @@ config.test_source_root = os.path.dirname(__file__)
 config.suffixes = ['.test']
 
 
+config.substitutions.append(('%target_ar_cmd', config.target_ar))
+
+
 config.substitutions.append(('%target_as_cmd', config.target_as))
 
 
 config.substitutions.append(('%target_ld_cmd', config.target_ld))
+
+
+config.substitutions.append(('%target_ranlib_cmd', config.target_ranlib))
 
 
 if config.target_run:

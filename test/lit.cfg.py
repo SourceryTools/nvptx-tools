69484561482bb3a4f6b5d25941f7a9315a1c3021
Thomas Schwinge <thomas@codesourcery.com>
2020-12-23 17:14:36 +0100

    [as] Add testcases for aspects of '--verify'/'--no-verify' etc. [#18]

--- test/lit.cfg.py
+++ test/lit.cfg.py
@@ -1,6 +1,23 @@
 # lit configuration
 
 import lit.formats
+import os
+import shutil
+import subprocess
+
+
+def which(cmd):
+    # 'shutil.which' is "New in version 3.3".
+    try:
+        return shutil.which(cmd)
+    except AttributeError:
+        # <https://stackoverflow.com/a/9877856>
+        for p in os.getenv('PATH').split(os.path.pathsep):
+            p=os.path.join(p, cmd)
+            if os.path.exists(p) and os.access(p, os.X_OK):
+                return p
+
+    return None
 
 
 config.name = 'nvptx-tools'
@@ -12,12 +29,30 @@ config.test_source_root = os.path.dirname(__file__)
 config.suffixes = ['.test']
 
 
+# This one is optional.
+config.ptxas = which('ptxas')
+
+
 config.substitutions.append(('%target_ar_cmd', config.target_ar))
 
 
 config.substitutions.append(('%target_as_cmd', config.target_as))
 
 config.substitutions.append(('%dummy_ptxas_path', 'PATH=' + config.test_source_root + '/as/ptxas:$PATH'))
+
+# Aside from different capitalization, the same error diagnostic is emitted by 'ptxas' vs. 'nvptx-none-as' internal checking (if 'ptxas' is not available).
+config.substitutions.append(('%r_target_as_missing_version_directive', '[mM]issing \.version directive at start of file'))
+
+if config.ptxas:
+    config.available_features.add('ptxas')
+
+    # Does 'ptxas' know about 'sm_30'?
+    # CUDA 11+ doesn't anymore: "ptxas fatal   : Value 'sm_30' is not defined for option 'gpu-name'".
+    # 'ptxas --version' output is preserved intentionally.
+    if subprocess.call(args = [config.ptxas, '--gpu-name', 'sm_30', '--version']) == 0:
+        config.available_features.add('ptxas_sm_30')
+    else:
+        subprocess.check_call(args = [config.ptxas, '--version'])
 
 
 config.substitutions.append(('%target_ld_cmd', config.target_ld))

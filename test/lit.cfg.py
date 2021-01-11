7d79c37658e9dba567c918dfc23f926161f2cc98
Thomas Schwinge <thomas@codesourcery.com>
2021-01-11 18:13:52 +0100

    [as] Verify more of the preamble as generated by GCC
    ... instead of just the initial '.version' token.

--- test/lit.cfg.py
+++ test/lit.cfg.py
@@ -40,8 +40,12 @@ config.substitutions.append(('%target_as_cmd', config.target_as))
 
 config.substitutions.append(('%dummy_ptxas_path', 'PATH=' + config.test_source_root + '/as/ptxas:$PATH'))
 
-# Error diagnostic as emitted by the 'nvptx-none-as' minimalistic verification.
+# Error diagnostics as emitted by the 'nvptx-none-as' minimalistic verification.
 config.substitutions.append(('%r_target_as_missing_version_directive', '^nvptx-as: missing \.version directive at start of file'))
+config.substitutions.append(('%r_target_as_malformed_version_directive', '^nvptx-as: malformed \.version directive at start of file'))
+config.substitutions.append(('%r_target_as_missing_target_directive', '^nvptx-as: missing \.target directive at start of file'))
+config.substitutions.append(('%r_target_as_malformed_target_directive', '^nvptx-as: malformed \.target directive at start of file'))
+config.substitutions.append(('%r_target_as_unsupported_list_in_target_directive', '^nvptx-as: unsupported list in \.target directive at start of file'))
 
 if config.ptxas:
     config.available_features.add('ptxas')

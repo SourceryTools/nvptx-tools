a458337c87d60c4e888fd4d8280a27bc347a1c7b
Thomas Schwinge <thomas@codesourcery.com>
2021-01-12 13:27:40 +0100

    [as] Document '-m' option
    This amends commit 2d5d59d8d65de3f06f3fd7c268568a70e7d1e2d7 "Add a -m switch to
    the assembler".

--- nvptx-as.c
+++ nvptx-as.c
@@ -77,6 +77,8 @@
 
 #define DIR_UP ".."
 
+#define SMVER_DEFAULT "sm_30"
+
 static const char *outname = NULL;
 
 static void __attribute__ ((format (printf, 1, 2)))
@@ -1052,7 +1054,7 @@ main (int argc, char **argv)
   FILE *out = stdout;
   bool verbose __attribute__((unused)) = false;
   int verify = -1;
-  const char *smver = "sm_30";
+  const char *smver = SMVER_DEFAULT;
 
   int o;
   int option_index = 0;
@@ -1087,6 +1089,8 @@ main (int argc, char **argv)
 	  printf ("\
 Usage: nvptx-none-as [option...] [asmfile]\n\
 Options:\n\
+  -m TARGET             Override %s target architecture used for ptxas\n\
+                        verification\n\
   -o FILE               Write output to FILE\n\
   -v                    Be verbose\n\
   --verify              Do verify output is acceptable to ptxas\n\
@@ -1095,6 +1099,7 @@ Options:\n\
   --version             Print version number and exit\n\
 \n\
 Report bugs to %s.\n",
+		  SMVER_DEFAULT,
 		  REPORT_BUGS_TO);
 	  exit (0);
 	case 'V':

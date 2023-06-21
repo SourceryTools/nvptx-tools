e64e50de642b4291667d670a9d91a43e5bfeafe1
Thomas Schwinge <thomas@codesourcery.com>
2023-06-21 12:01:13 +0200

    'test/GLOBAL_FUNCTION_DECL_f.s': 'f' is '.extern', not '.visible' [#38]
    Recent versions of 'ptxas' reject a '.visible' function declaration without
    accompanying definition:
    
        [...]
        FAIL: nvptx-tools :: ld/search-1.test (11 of 35)
        [...]
        + : 'RUN: at line 13'
        + [...]/nvptx-none-as -o [...]/test/ld/Output/search-1.test.tmp/o/GLOBAL_FUNCTION_DECL_f.o [...]/test/ld/../GLOBAL_FUNCTION_DECL_f.s
        ptxas fatal   : Unresolved extern function 'f'
        nvptx-as: ptxas returned 255 exit status
        [...]
    
    ..., and indeed '.extern' is what GCC/nvptx generates for
    'void f(); void *g = f;', for example.
    
    Fix-up for #38 commit 02a433cae5a4355d14bdac16dd156cc5fb017843
    'ld: Document current "search" behavior'.

--- test/GLOBAL_FUNCTION_DECL_f.s
+++ test/GLOBAL_FUNCTION_DECL_f.s
@@ -5,4 +5,4 @@
 // END PREAMBLE
 
 // BEGIN GLOBAL FUNCTION DECL: f
-.visible .func (.param.u32 %value_out) f (.param.u32 %in_ar0, .param.u32 %in_ar1);
+.extern .func (.param.u32 %value_out) f (.param.u32 %in_ar0, .param.u32 %in_ar1);

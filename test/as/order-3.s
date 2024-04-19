6c208239e32da9b65ced03e536ccec441dd83363
Thomas Schwinge <tschwinge@baylibre.com>
2024-04-19 08:53:31 +0200

    as: Document current handling of '//:IND_FUNC_MAP' [#45]

--- test/as/order-3.s
+++ test/as/order-3.s
@@ -5,6 +5,7 @@
 //:FUNC_MAP "f1.1"
 // comment 1
 //:FUNC_MAP "f1.2"
+//:IND_FUNC_MAP "if1.2"
 //:VAR_MAP "v1"
 .visible .entry e1 ()
 {
@@ -15,6 +16,8 @@
 //:FUNC_MAP "f2"
 // comment 2.2
 //:VAR_MAP "v2"
+// comment 2.3
+//:IND_FUNC_MAP "if2"
 .visible .entry e2 ()
 {
 	// [...]
@@ -29,6 +32,7 @@
 
 // The following form is never emitted by GCC, but still happens to be handled here ("by accident"):
 /*:FUNC_MAP "f4"
+:IND_FUNC_MAP "if4"
 :VAR_MAP "v4" */
 .visible .entry e4 ()
 {
@@ -40,3 +44,11 @@
 {
 	// [...]
 }
+
+//:IND_FUNC_MAP "if6"
+.visible .entry e6 ()
+{
+	// [...]
+}
+
+//:IND_FUNC_MAP "if7"
